f6dce94227d694b8d61efbcfa36783217c49c697
Li, Chenggang <lcgblog@163.com>
2020-11-30 23:19:41 +0800

    update script

--- tool-scripts/ubuntu-18.04/init-apt-source.sh
+++ tool-scripts/ubuntu-18.04/init-apt-source.sh
@@ -10,21 +10,19 @@ fi
 
 mv /etc/apt/sources.list /etc/apt/sources.list.bak
 
-core_version=$(lsb_release -c --short)
-export core_version
 
 tee /etc/apt/sources.list <<-'EOF'
-deb http://mirrors.aliyun.com/ubuntu/ ${core_version} main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ ${core_version} main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ ${core_version}-security main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ ${core_version}-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
 
-deb http://mirrors.aliyun.com/ubuntu/ ${core_version}-updates main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ ${core_version}-updates main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ ${core_version}-backports main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ ${core_version}-backports main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ ${core_version}-proposed main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ ${core_version}-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
 EOF
 
 apt-get update
