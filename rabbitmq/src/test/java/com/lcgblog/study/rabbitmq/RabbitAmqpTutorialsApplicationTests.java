ef516594d10d7a20ad807a5dad6ed407ae9d27c0
Li, Chenggang <lcgblog@163.com>
2021-07-22 00:10:10 +0800

    STUD-119 complete tut1

---
+++ rabbitmq/src/test/java/com/lcgblog/study/rabbitmq/RabbitAmqpTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcgblog.study.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitAmqpTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
