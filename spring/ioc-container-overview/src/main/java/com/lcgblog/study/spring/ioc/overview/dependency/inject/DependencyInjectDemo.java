e6d080e1d2b6197b3a911d90a9fbb40d7e0f5c74
Elink <512856803@qq.com>
2021-09-13 08:34:54 +0800

    dependency lookup and injection

--- spring/ioc-container-overview/src/main/java/com/lcgblog/study/spring/ioc/overview/dependency/inject/DependencyInjectDemo.java
+++ spring/ioc-container-overview/src/main/java/com/lcgblog/study/spring/ioc/overview/dependency/inject/DependencyInjectDemo.java
@@ -2,8 +2,7 @@ package com.lcgblog.study.spring.ioc.overview.dependency.inject;
 
 import com.lcgblog.study.spring.ioc.overview.repository.UserRepository;
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DependencyInjectDemo {
@@ -12,8 +11,7 @@ public class DependencyInjectDemo {
         UserRepository userRepository = beanFactory.getBean(UserRepository .class);
         System.out.println(userRepository.getUserList());
         System.out.println(userRepository.getBeanFactory() == beanFactory); // false
-        System.out.println(userRepository.getBeanFactory() == ((ApplicationContext)beanFactory).getParentBeanFactory()); // false
-        System.out.println(userRepository.getBeanFactory() == ((AbstractApplicationContext)beanFactory).getBeanFactory()); // true
+        System.out.println(userRepository.getBeanFactory() == ((ConfigurableApplicationContext)beanFactory).getBeanFactory()); // true
         //because application is not BeanFactory itself, it just implements the BeanFactory interface
         // and combines with a real beanFactory to realize the BeanFactory interface
     }
