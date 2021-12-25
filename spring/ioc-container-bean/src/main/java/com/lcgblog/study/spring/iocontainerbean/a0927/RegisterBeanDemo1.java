6213fb180dfd0ee11f2b70ed5f8ffabbdd1e6490
Chenggang, li <lcgblog@126.com>
2021-12-25 21:41:09 +0800

    完善验证case

--- spring/ioc-container-bean/src/main/java/com/lcgblog/study/spring/iocontainerbean/a0927/RegisterBeanDemo1.java
+++ spring/ioc-container-bean/src/main/java/com/lcgblog/study/spring/iocontainerbean/a0927/RegisterBeanDemo1.java
@@ -3,12 +3,16 @@ package com.lcgblog.study.spring.iocontainerbean.a0927;
 import com.lcgblog.study.spring.ioc.overview.domain.User;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
 
 //注册bean
-//3.通过@Import导入Config
+//3.通过@Import导入Config,精确引入
 @Import({RegisterBeanDemo1.Config.class})
+//@Import({RegisterBeanDemo1.Config.class, RegisterBeanDemo1.Config2.class})
+//4.全部引入
+//@Configuration
 public class RegisterBeanDemo1 {
 
     public static void main(String[] args) {
@@ -19,6 +23,7 @@ public class RegisterBeanDemo1 {
         applicationContext.refresh();
         System.out.println(applicationContext.getBeansOfType(RegisterBeanDemo1.class));
         System.out.println(applicationContext.getBeansOfType(Config.class));
+        System.out.println(applicationContext.getBeansOfType(Config2.class));
         System.out.println(applicationContext.getBeansOfType(User.class));
 
 
@@ -41,6 +46,18 @@ public class RegisterBeanDemo1 {
             User user = new User();
             user.setId(1L);
             user.setName("zs");
+            return user;
+        }
+    }
+
+    @Component
+    public static class Config2{
+        //1.通过@Bean注册bean
+        @Bean
+        public User user2(){
+            User user = new User();
+            user.setId(2L);
+            user.setName("ls");
             return user;
         }
     }
