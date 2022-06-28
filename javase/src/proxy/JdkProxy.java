ae3c8cb11a6eafd9fd54c7abb3259913bf04c666
Chenggang, li <lcgblog@126.com>
2022-06-28 14:34:23 +0800

    add jdk dynamic proxy

--- javase/src/proxy/JdkProxy.java
+++ javase/src/proxy/JdkProxy.java
@@ -1,2 +1,41 @@
-package proxy;public class JdkProxy {
+package proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class JdkProxy {
+
+    public static void main(String[] args) {
+        UserImpl user = new UserImpl();
+        Object userProxy = Proxy.newProxyInstance(UserImpl.class.getClassLoader(),
+                UserImpl.class.getInterfaces(), new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                Object result = null;
+                System.out.println("洗手");
+                result = method.invoke(user);
+                System.out.println("睡觉");
+                return result;
+            }
+        });
+        ((User)userProxy).eat();
+        ((User2)userProxy).eat2();
+    }
+
+    static class UserImpl implements User,User2{
+        public void eat(){
+            System.out.println("吃饭");
+        }
+        public void eat2(){
+            System.out.println("吃饭2");
+        }
+    }
+
+    interface User {
+        public void eat();
+    }
+    interface User2 {
+        public void eat2();
+    }
 }
