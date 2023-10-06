850e4df38bd876ecac34cd6d05486ee2d70bb0cb
Chenggang, li <lcgblog@126.com>
2023-10-07 00:38:00 +0800

    upgrade jdk 17

--- springboot/src/main/java/com/lcgblog/study/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/lcgblog/study/springboot/SpringbootApplication.java
@@ -1,19 +1,13 @@
 package com.lcgblog.study.springboot;
 
-import org.apache.ignite.configuration.IgniteConfiguration;
-import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
-		IgniteConfiguration cfg = new IgniteConfiguration();
-		cfg.setDiscoverySpi(new TcpDiscoverySpi());
 	}
 
 }
--- springboot/src/main/java/com/lcgblog/study/springboot/plugins/impl/SecurityPlugin.java
+++
@@ -1,7 +0,0 @@
-package com.lcgblog.study.springboot.plugins.impl;
-
-import org.apache.ignite.plugin.IgnitePlugin;
-
-public class SecurityPlugin implements IgnitePlugin {
-
-}
