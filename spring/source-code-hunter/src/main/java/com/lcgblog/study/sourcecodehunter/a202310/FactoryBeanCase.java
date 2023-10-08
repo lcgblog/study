5624c740ff5370b9fe2ef5f0756b496143f0fb17
Chenggang, li <lcgblog@126.com>
2023-10-08 17:02:00 +0800

    add sample for source code hunter

--- spring/source-code-hunter/src/main/java/com/lcgblog/study/sourcecodehunter/a202310/FactoryBeanCase.java
+++ spring/source-code-hunter/src/main/java/com/lcgblog/study/sourcecodehunter/a202310/FactoryBeanCase.java
@@ -1,2 +1,48 @@
-package com.lcgblog.study.sourcecodehunter.a202310;public class FactoryBeanCase {
+package com.lcgblog.study.sourcecodehunter.a202310;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FactoryBeanCase {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(FactoryBeanCase.class);
+        System.out.println(applicationContext.getBean("myFactoryBean"));
+        System.out.println(applicationContext.getBean("&myFactoryBean"));
+    }
+
+    @Bean
+    public MyFactoryBean myFactoryBean(){
+        return new MyFactoryBean();
+    }
+
+    public static class User{
+        @Override
+        public String toString() {
+            return "User";
+        }
+    }
+
+    public static class MyFactoryBean implements FactoryBean<User> {
+
+        @Override
+        public User getObject() throws Exception {
+            return new User();
+        }
+
+        @Override
+        public Class<?> getObjectType() {
+            return User.class;
+        }
+
+        @Override
+        public String toString() {
+            return "MyFactoryBean";
+        }
+    }
 }
--- spring/source-code-hunter/src/main/java/com/lcgblog/study/sourcecodehunter/a202310/ResolvableTypeCase.java
+++ spring/source-code-hunter/src/main/java/com/lcgblog/study/sourcecodehunter/a202310/ResolvableTypeCase.java
@@ -1,2 +1,30 @@
-package com.lcgblog.study.sourcecodehunter.a202310;public class ResolvableTypeCase {
+package com.lcgblog.study.sourcecodehunter.a202310;
+
+import org.springframework.beans.SimpleTypeConverter;
+import org.springframework.beans.TypeConverter;
+import org.springframework.core.ResolvableType;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResolvableTypeCase {
+
+    public static void main(String[] args) {
+        ResolvableType resolvableType = ResolvableType.forClass(User.class);
+        System.out.println(resolvableType.getSuperType());
+        System.out.println(resolvableType.getInterfaces()[0]);
+        System.out.println(resolvableType.resolveGeneric());
+        System.out.println(resolvableType.resolve());
+        Class<? extends List> targetListClass = UserList.class;
+        System.out.println(ResolvableType.forClass(targetListClass).asCollection().resolveGeneric());
+
+    }
+
+    static class Parent{}
+
+    static class User<T extends List<Parent>> extends Parent implements Serializable {
+    }
+
+    static class UserList extends ArrayList<Parent>{}
 }
